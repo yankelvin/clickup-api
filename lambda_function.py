b0cc23c72033f7cceb7635e115004b4f330cc5b6
Yan Kelvin <yan.kelvin@hotmail.com>
2022-08-25 23:47:12 -0300

    correções no lambda

--- lambda_function.py
+++ lambda_function.py
@@ -1,35 +1,27 @@
-import json
 import os
-import asyncio
 from services.Engine import Engine
+from services.MongoDB import MongoDB
 
 authorization = os.environ.get('AUTHORIZATION', None)
-engine = Engine(authorization=authorization)
+connectionString = os.environ.get('CONNECTION_STRING', None)
 
-loop = asyncio.get_event_loop()
+team_name = os.environ.get('TEAM_NAME', None)
+space_name = os.environ.get('SPACE_NAME', None)
+folder_name = os.environ.get('FOLDER_NAME', None)
+
+engine = Engine(authorization=authorization)
+mongodb = MongoDB(connectionString=connectionString)
 
 
 def lambda_handler(event, context):
     try:
-        print(event)
+        tasks = engine.get_tasks(team_name=team_name, space_name=space_name,
+                                 folder_name=folder_name, list_name=None)
 
-        query = event["queryStringParameters"]
-        team_name = query["team_name"]
-        space_name = query["space_name"]
-        folder_name = query["folder_name"]
-        list_name = None
-        
-        if "list_name" in query:
-            list_name = query["list_name"]
+        mongodb.delete_all_tasks()
+        mongodb.insert_tasks(tasks)
 
-        tasks = loop.run_until_complete(engine.get_tasks(team_name=team_name, space_name=space_name,
-                                                         folder_name=folder_name, list_name=list_name))
-
-        return {
-            "statusCode": 200,
-            "headers": {},
-            "body": json.dumps(tasks)
-        }
+        return f"Tasks inseridas: {len(tasks)}"
     except Exception as ex:
         print(ex)
         return ex
--- services/TaskApi.py
+++ services/TaskApi.py
@@ -1,7 +1,6 @@
-import asyncio
-import aiohttp
-from time import time
+import requests
 from typing import List
+
 from models.Task import Task
 
 
@@ -9,41 +8,28 @@ class TaskApi:
     def __init__(self, authorization: str) -> None:
         self.authorization = authorization
 
-    async def get_tasks(self, list_id: str) -> List[Task]:
-        tasks: List[Task] = []
+    def get_tasks(self, list_id: str) -> List[Task]:
         page = 0
         finish = False
-
-        urls = []
+        tasks: List[Task] = []
 
         while not finish:
             url = f"https://api.clickup.com/api/v2/list/{list_id}/task?subtasks=true&include_closed=true&page={page}"
-            headers = {"Authorization": self.authorization}
+            response_tasks = self.get(url=url)
 
-            urls.append(url)
+            if len(response_tasks) == 0:
+                finish = True
+                break
+
+            tasks = tasks + response_tasks
             page += 1
-
-            if (page + 1) % 5 == 0:
-                async with aiohttp.ClientSession(headers=headers, connector=aiohttp.TCPConnector(verify_ssl=False)) as session:
-                    response_tasks = await asyncio.gather(*[self.get(url, session) for url in urls])
-                urls = []
-            else:
-                continue
-
-            for response in response_tasks:
-                if len(response["tasks"]) == 0:
-                    finish = True
-                    break
-                else:
-                    tasks = tasks + response["tasks"]
 
         return tasks
 
-    async def get(self, url, session):
-        try:
-            print(url)
-            async with session.get(url=url) as response:
-                resp = await response.json()
-                return resp
-        except Exception as e:
-            print("Unable to get url {} due to {}.".format(url, e.__class__))
+    def get(self, url):
+        headers = {"Authorization": self.authorization}
+        response = requests.request(
+            "GET", url, data="", headers=headers)
+        response_tasks = response.json()["tasks"]
+
+        return response_tasks
